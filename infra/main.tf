78ce3c19de6d51f9d4bbb40c6f0685e0ec2b59da
Mebu98 <merijn.buitelaar98@gmail.com>
2023-11-23 19:17:10 +0000

    fixed apprunner cpu and memory

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,10 @@ resource "aws_apprunner_service" "service" {
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    
+    cpu = 256
+    
+    memory = 1024
   }
 
   source_configuration {
