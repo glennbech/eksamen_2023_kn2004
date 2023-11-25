0a7e9d4a59fc063963a8afca394a9578497f23fd
Mebu98 <merijn.buitelaar98@gmail.com>
2023-11-25 00:56:50 +0000

    forgot to declare a variable...

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "img" {
     description = "The identifier of the image to use."
     default = "kn2004-ppe:latest"
 }
+
+variable "dashboard_name"{
+    description = "Dashboard name."
+    default = "kn2004-rekognition-dashboard"
+}
